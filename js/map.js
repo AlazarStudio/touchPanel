2e8a753cd98827e3fcbddf0db53fa1a30a4ab0f3
AlazarStudio <159021509+AlazarStudio@users.noreply.github.com>
2024-03-20 16:12:13 +0300

    Animations

---
+++ js/map.js
@@ -0,0 +1,98 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+  setTimeout(function () {
+    document.querySelector(".line").classList.add("line-animate");
+    document.querySelector(".line2").classList.add("line2-animate");
+    document.querySelector(".text").classList.add("text-animate");
+  }, 100);
+
+  setTimeout(function () {
+    document
+      .querySelector(".mark_prikub .line2")
+      .classList.add("mark_prikub-lines-animate");
+    document
+      .querySelector(".mark_prikub .text")
+      .classList.add("mark_prikub-text-animates");
+  }, 200);
+
+  setTimeout(function () {
+    document
+      .querySelector(".mark_habl .line2")
+      .classList.add("mark_habl-lines-animate");
+    document
+      .querySelector(".mark_habl .text")
+      .classList.add("mark_habl-text-animates");
+  }, 300);
+
+  setTimeout(function () {
+    document
+      .querySelector(".mark_abaz .line2")
+      .classList.add("mark_abaz-lines-animate");
+    document
+      .querySelector(".mark_abaz .text")
+      .classList.add("mark_abaz-text-animates");
+  }, 400);
+
+  setTimeout(function () {
+    document
+      .querySelector(".mark_nogai .line2")
+      .classList.add("mark_nogai-lines-animate");
+    document
+      .querySelector(".mark_nogai .text")
+      .classList.add("mark_nogai-text-animates");
+  }, 500);
+
+  setTimeout(function () {
+    document
+      .querySelector(".mark_habez .line2")
+      .classList.add("mark_habez-lines-animate");
+    document
+      .querySelector(".mark_habez .text")
+      .classList.add("mark_habez-text-animates");
+  }, 600);
+
+  setTimeout(function () {
+    document
+      .querySelector(".mark_ust .line2")
+      .classList.add("mark_ust-lines-animate");
+    document
+      .querySelector(".mark_ust .text")
+      .classList.add("mark_ust-text-animates");
+  }, 700);
+
+  setTimeout(function () {
+    document
+      .querySelector(".mark_malkar .line2")
+      .classList.add("mark_malkar-lines-animate");
+    document
+      .querySelector(".mark_malkar .text")
+      .classList.add("mark_malkar-text-animates");
+  }, 800);
+
+  setTimeout(function () {
+    document
+      .querySelector(".mark_urup .line2")
+      .classList.add("mark_urup-lines-animate");
+    document
+      .querySelector(".mark_urup .text")
+      .classList.add("mark_urup-text-animates");
+  }, 900);
+
+  setTimeout(function () {
+    document
+      .querySelector(".mark_zelen .line2")
+      .classList.add("mark_zelen-lines-animate");
+    document
+      .querySelector(".mark_zelen .text")
+      .classList.add("mark_zelen-text-animates");
+  }, 1000);
+
+  setTimeout(function () {
+    document
+      .querySelector(".mark_karach .line2")
+      .classList.add("mark_karach-lines-animate");
+    document
+      .querySelector(".mark_karach .text")
+      .classList.add("mark_karach-text-animates");
+  }, 1100);
+});
